d71625edaae771d103bf4b7fc99f1b615949c56d
Rish2103 <rishabh3989@gmail.com>
2026-07-05 12:00:18 +0530

    streamlit setup + homescreen

---
+++ src/screens/components/footer.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+def footer_home():
+
+    # --- FOOTER SECTION ---
+    st.markdown("---")
+
+    st.markdown(
+        """
+        <div style="text-align: center; padding: 10px; color: #ffffff; font-size: 14px;">
+            Created with ❤️ by <span style="font-weight: bold; color: #071645;">Rishabh</span>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
---
+++ src/screens/student_screen.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+def student_screen():
+    st.title("Student Screen")
+    st.write("Welcome, Student! Here you can view your assignments and grades.")
